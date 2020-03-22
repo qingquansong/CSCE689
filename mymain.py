049a92fbff7f4a876d78f756c1667c8cac968fb3
song3134 <song_3134@tamu.edu>
2020-03-21 22:10:44 -0700

    update hw4

--- mymain.py
+++ mymain.py
@@ -41,51 +41,52 @@ from datasets.ucf101 import UCF101
 def set_opts():
     
     opt = easydict.EasyDict({
-        "result_path": 'results2',
-        "dataset": 'ucf101-music', # 'ucf101',
-        "n_classes": 9, 
-        "sample_size": 112,
-        "sample_duration": 16,
-        "initial_scale": 1.0,
-        "n_scales": 5,
-        "scale_step": 0.84089641525,
-        "train_crop": 'corner',
-        "learning_rate": 0.1,
-        "momentum": 0.9,
-        "dampening": 0.9,
-        "weight_decay": 0.001,
-        "mean_dataset": 'activitynet',
-        "no_mean_norm": False,
-        "std_norm": False,
-        "nesterov": False,
-        "optimizer": 'sgd',
-        "lr_patience": 10,
-        "batch_size": 32,
-        "n_epochs": 2,
-        "begin_epoch": 1,
-        "n_val_samples": 3,
-        "ft_begin_index": 5,
-        "scale_in_test": 1.0,
-        "crop_position_in_test": 'c',
-        "no_softmax_in_test": False,
-        "no_cuda": False,
-        "n_threads": 4,
-        "checkpoint": 2,
-        "no_hflip": False,
-        "norm_value": 1,
-        "model": 'resnet',
+        "result_path": 'results1',        # 'Result directory path'
+        "dataset": 'ucf101-music',      # 'Used dataset‘
+        "n_classes": 9,                 # 'Number of classes for fine-tuning'
+        "sample_size": 112,             # 'Height and width of inputs'
+        "sample_duration": 16,          # 'Temporal duration of inputs'
+        "initial_scale": 1.0,           # 'Initial scale for multiscale cropping')
+        "n_scales": 5,                  # 'Number of scales for multiscale cropping'
+        "scale_step": 0.84089641525,    # 'Scale step for multiscale cropping'
+        "train_crop": 'corner',         # 'Spatial cropping method in training. Corner is selection from 4 corners and 1 center.
+        "learning_rate": 0.1,           # 'Initial learning rate (divided by 10 while training by lr scheduler)'
+        "momentum": 0.9,                # 'Momentum'
+        "dampening": 0.9,               # 'Dampening of SGD'
+        "weight_decay": 0.001,          # 'Weight decay in SGD'
+        "mean_dataset": 'kinetics',     # 'Dataset for mean values of mean subtraction'
+        "no_mean_norm": False,          # 'If true, inputs are not normalized by mean'
+        "std_norm": False,              # 'If true, inputs are normalized by standard deviation'
+        "nesterov": False,              # 'Nesterov momentum'
+        "optimizer": 'sgd',             # 'Optimizer type'
+        "lr_patience": 10,              # 'Patience of LR scheduler. See documentation of Reduce on Loss Plateau Decay (ReduceLROnPlateau). https://www.deeplearningwizard.com/deep_learning/boosting_models_pytorch/lr_scheduling/#reduce-on-loss-plateau-decay'
+        "batch_size": 32,               # 'Batch size'
+        "n_epochs": 100,                  # 'Fine-tuning epochs'
+        "begin_epoch": 1,               # 'Training begins at this epoch. Previous trained model indicated by resume_path is loaded.'
+        "n_val_samples": 3,             # 'Number of validation samples for each activity'
+        "ft_begin_index": 5,            # 'Begin block index of fine-tuning'
+        "scale_in_test": 1.0,           # 'Spatial scale in test'
+        "crop_position_in_test": 'c',   #  Cropping method: center cropping
+        "no_softmax_in_test": False,    # 'If true, output for each clip is not normalized using softmax.'
+        "no_cuda": False,               # 'If true, cuda is not used.'
+        "n_threads": 4,                 # 'Number of threads for multi-thread loading'
+        "checkpoint": 10,                #  Trained model is saved at every this epochs'
+        "no_hflip": False,              # 'If true holizontal flipping is not performed'
+        "norm_value": 1,                # 'If 1, range of inputs is [0-255]. If 255, range of inputs is [0-1].'
+        "model": 'resnet',              
         "pretained_model_name": 'resnext-101-kinetics',
-        "model_depth": 101,
-        "resnet_shortcut": 'B',
-        "wide_resnet_k": 2,
-        "resnext_cardinality": 32,
-        "manual_seed": 1,
-        'test_subset': 'test',
+        "model_depth": 101,             # 'Depth of resnet 
+        "resnet_shortcut": 'B',         # 'Shortcut type of resnet' to align the dimensionality for skip connection
+        "wide_resnet_k": 2,             # 'Wide resnet k'
+        "resnext_cardinality": 32,      # 'ResNeXt cardinality'
+        "manual_seed": 1,               # 'Manually set random seed'
+        'test_subset': 'test',          # 'Used test subset name'
     })
     opt.arch = '{}-{}'.format(opt.model, opt.model_depth)
-    opt.root_path = '/data/qq/CSCE689/video/'
-    opt.video_path = opt.root_path + 'UCF-music/'
-    opt.annotation_path = opt.root_path+'UCF-music-annotation/ucf101_music_with_testing.json'
+    opt.root_path = '/data/qq/CSCE689/'
+    opt.video_path = opt.root_path + 'video/UCF-music/'
+    opt.annotation_path = opt.root_path + 'video/UCF-music-annotation/ucf101_music_with_testing.json'
+    opt.pretrained_model_path = opt.root_path + 'pretrain/' + opt.pretained_model_name + '.pth'
 
     return opt
 
@@ -103,7 +104,8 @@ def load_pretrained_resnet101(opt):
     # wrap the current model again in nn.DataParallel / or we can just remove the .module keys.
     model = nn.DataParallel(model, device_ids=None)
     
-    pretrain = torch.load('/data/qq/CSCE689/pretrain/' + opt.pretained_model_name + '.pth')
+    # Note: please ccustomize the pretrained model path
+    pretrain = torch.load(opt.pretrained_model_path)
     pretrain_dict = pretrain['state_dict']
 
     # do not load the last layer
@@ -171,6 +173,7 @@ def get_ucf_data(opt):
         pin_memory=False) 
 
     
+    target_transform = VideoID()
     # get test data
     test_data = UCF101(
         opt.video_path,
@@ -191,13 +194,13 @@ def get_ucf_data(opt):
         num_workers=opt.n_threads,
         pin_memory=False)
     
-    return train_loader, val_loader, test_loader
+    return train_loader, val_loader, test_loader, test_data
 
 def main():
     
     opt = set_opts()
     model = load_pretrained_resnet101(opt)
-    train_loader, val_loader, test_loader = get_ucf_data(opt)
+    train_loader, val_loader, test_loader, test_data = get_ucf_data(opt)
     
     criterion = nn.CrossEntropyLoss()
     if not opt.no_cuda:
@@ -226,6 +229,7 @@ def main():
     val_logger = Logger(
                 os.path.join(opt.result_path, 'val.log'), ['epoch', 'loss', 'acc'])
 
+    # training
     for i in range(opt.begin_epoch, opt.n_epochs + 1):
 
         train_epoch(i, train_loader, model, criterion, optimizer, 
@@ -235,6 +239,9 @@ def main():
                                     opt, val_logger)
 
         scheduler.step(validation_loss)
+
+    # testing
+    test_results, all_output_buffer = final_test(test_loader, model, opt, test_data.class_names)
         
 if __name__ == '__main__':
     os.environ['CUDA_VISIBLE_DEVICES']='2,3'
